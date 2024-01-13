af200d2dc2c2e79e89b728dccec4d3c6743e349d
Metta Surendhar <msurendhar8815@gmail.com>
2024-01-13 16:13:42 +0530

    completed the image identifying implementation

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
 	const [isModelLoading, setIsModelLoading] = useState(false);
 	const [model, setModel] = useState(null);
 	const [imageUrl, setImageUrl] = useState(null);
+	const [isImageValid, setIsImageValid] = useState(false);
 	const [results, setResults] = useState(null);
 
 	const imageRef = useRef();
+	const textInputRef = useRef();
+	const fileInputRef = useRef();
 
 	const load = async () => {
 		setIsModelLoading(true);
@@ -24,17 +27,54 @@ function App() {
 	};
 
 	const uploadImage = (e) => {
+		textInputRef.current.value = '';
 		const { files } = e.target;
 		if (files.length > 0) {
+			setIsImageValid(true);
 			setImageUrl(URL.createObjectURL(files[0]));
 		} else {
+			setIsImageValid(false);
 			setImageUrl(null);
 		}
 	};
 
 	const identify = async () => {
+		textInputRef.current.value = '';
+		fileInputRef.current.value = '';
 		const results = await model.classify(imageRef.current);
 		setResults(results);
+	};
+
+	const cancel = async () => {
+		textInputRef.current.value = '';
+		fileInputRef.current.value = '';
+		setIsImageValid(false);
+	};
+
+	const validateImageUrl = (url) => {
+		return new Promise((resolve, reject) => {
+			const img = new Image();
+			img.onload = () => resolve(true);
+			img.onerror = () => reject(false);
+			img.src = url;
+		});
+	};
+
+	const handleOnChange = (e) => {
+		fileInputRef.current.value = '';
+		validateImageUrl(e.target.value)
+			.then((result) => {
+				setIsImageValid(true);
+			})
+			.catch((err) => {
+				setIsImageValid(false);
+			});
+		setImageUrl(e.target.value);
+		setResults([]);
+	};
+
+	const triggerUpload = () => {
+		fileInputRef.current.click();
 	};
 
 	useEffect(() => {
@@ -50,17 +90,32 @@ function App() {
 			<h1 className='header'>Image Classification</h1>
 			<div className='inputHolder'>
 				<input
-					className='uploadInput'
+					className='uploadFileInput'
 					type='file'
 					accept='image/*'
 					capture='camera'
+					ref={fileInputRef}
 					onChange={uploadImage}
+				/>
+				<button
+					className='uploadImg'
+					onClick={triggerUpload}
+				>
+					upload image
+				</button>
+				<span className='or'> OR </span>
+				<input
+					className='uploadTextInput'
+					type='text'
+					placeholder='Paster Image URL'
+					ref={textInputRef}
+					onChange={handleOnChange}
 				/>
 			</div>
 			<div className='mainWrapper'>
 				<div className='mainContent'>
 					<div className='imageHolder'>
-						{imageUrl && (
+						{isImageValid && imageUrl && (
 							<img
 								src={imageUrl}
 								alt='Upload preview'
@@ -69,13 +124,21 @@ function App() {
 							/>
 						)}
 					</div>
-					{imageUrl && (
-						<button
-							className='button'
-							onClick={identify}
-						>
-							Identify Image
-						</button>
+					{isImageValid && imageUrl && (
+						<div className='buttonHolder'>
+							<button
+								className='button'
+								onClick={identify}
+							>
+								Identify
+							</button>
+							<button
+								className='button cancel'
+								onClick={cancel}
+							>
+								cancel
+							</button>
+						</div>
 					)}
 				</div>
 				{results !== null && results.length > 0 && (
